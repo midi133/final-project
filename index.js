ffca7724070eabe840b8ba707802cc121127dba5
YH <calpispig@gmail.com>
2026-06-09 02:54:01 +0800

    修正

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const menu = document.querySelector('.menu-')
-const navlist = document.querySelector('.nav-list')
+/*漢堡選單*/ 
+const menuBtn = document.querySelector('.menu-btn') /*行動版漢堡按鈕*/ 
+const navlist = document.querySelector('.nav-list') /*主導覽選單元素*/ 
 
-menu.addEventListener('click', function () {
-    navlist.classList.toggle('active')
-    document.body.classList.toggle('menu-open')
+menuBtn.addEventListener('click', function () {
+    navlist.classList.toggle('active')              /*有 active 就移除，沒有就加上*/
+    document.body.classList.toggle('menu-open')     /*有 menu-open 就移除，沒有就加上*/
     })
-
